6723d403597950215cf6714b02469e58daefd268
Azrty <thierry.ouellet@icloud.com>
2018-05-15 17:03:53 +0000

    boom!

---
+++ assets/js/front.js
@@ -0,0 +1,100 @@
+$(function () {
+
+    rotatingText();
+
+});
+
+/* =========================================
+ *  rotating text
+ *  =======================================*/
+
+function rotatingText() {
+
+    $("#intro .rotate").textrotator({
+        animation: "fade",
+        speed: 3000
+    });
+}
+
+/* animations */
+
+function animations() {
+    delayTime = 0;
+    $('[data-animate]').css({opacity: '0'});
+    $('[data-animate]').waypoint(function (direction) {
+        delayTime += 150;
+        $(this).delay(delayTime).queue(function (next) {
+            $(this).toggleClass('animated');
+            $(this).toggleClass($(this).data('animate'));
+            delayTime = 0;
+            next();
+            //$(this).removeClass('animated');
+            //$(this).toggleClass($(this).data('animate'));
+        });
+    },
+            {
+                offset: '90%',
+                triggerOnce: true
+            });
+
+    $('[data-animate-hover]').hover(function () {
+        $(this).css({opacity: 1});
+        $(this).addClass('animated');
+        $(this).removeClass($(this).data('animate'));
+        $(this).addClass($(this).data('animate-hover'));
+    }, function () {
+        $(this).removeClass('animated');
+        $(this).removeClass($(this).data('animate-hover'));
+    });
+
+}
+
+$.fn.alignElementsSameHeight = function () {
+    $('.same-height-row').each(function () {
+
+        var maxHeight = 0;
+        var children = $(this).find('.same-height');
+        children.height('auto');
+        if ($(window).width() > 768) {
+            children.each(function () {
+                if ($(this).innerHeight() > maxHeight) {
+                    maxHeight = $(this).innerHeight();
+                }
+            });
+            children.innerHeight(maxHeight);
+        }
+
+        maxHeight = 0;
+        children = $(this).find('.same-height-always');
+        children.height('auto');
+        children.each(function () {
+            if ($(this).height() > maxHeight) {
+                maxHeight = $(this).innerHeight();
+            }
+        });
+        children.innerHeight(maxHeight);
+
+    });
+}
+
+var windowWidth;
+
+$(window).load(function () {
+
+    windowWidth = $(window).width();
+
+    $(this).alignElementsSameHeight();
+
+});
+$(window).resize(function () {
+
+    newWindowWidth = $(window).width();
+
+    if (windowWidth !== newWindowWidth) {
+        setTimeout(function () {
+            $(this).alignElementsSameHeight();
+        }, 205);
+        windowWidth = newWindowWidth;
+    }
+
+});
